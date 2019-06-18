1ddc0b6dc27014058bb5d1be4ae9dd375cbc200a
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-06-17 22:00:26 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(6, 'out');
+var status = false;
+
+const INTERVAL = 1000;
+
+setInterval(()=> {
+    console.log(status ? '[ON]' : '[OFF]')
+    //status ? led.writeSync(1) : led.writeSync(0);
+    status = !status;
+}, INTERVAL);
