1cfaaa17a23858ea8b88d125ca10614b20f87d6f
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-06-17 22:28:45 -0600

    cambio a pin 21

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Gpio = require('onoff').Gpio;
-var led = new Gpio(27, 'out');
+var led = new Gpio(21, 'out');
 var status = false;
 
 const INTERVAL = 1000;
 
 setInterval(()=> {
     console.log(status ? '[ON]' : '[OFF]')
-    //status ? led.writeSync(1) : led.writeSync(0);
+    status ? led.writeSync(1) : led.writeSync(0);
     status = !status;
 }, INTERVAL);
