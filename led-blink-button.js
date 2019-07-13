34f7d117bcede5b9c6561910a3af33a7c3ec38a8
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-07-13 14:02:22 -0600

    led intermitente por 5s

--- led-blink-button.js
+++ led-blink-button.js
@@ -1,4 +1,4 @@
-const Gpio = require('../onoff').Gpio; // Gpio class
+const Gpio = require('onoff').Gpio; // Gpio class
 const led = new Gpio(17, 'out');       // Export GPIO17 as an output
  
 // Toggle the state of the LED connected to GPIO17 every 200ms
