bf83cb9614636581938b07a70292c4ca719fadcb
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-07-13 13:51:24 -0600

    log en consola fix

--- led-and-button.js
+++ led-and-button.js
@@ -3,20 +3,15 @@ const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
 
 console.log('Programa iniciado, presiona el botón para encender el led.');
-
-val = 0;
  
 button.watch((err, value) => {
   if (err) {
     throw err;
   }
 
-  val += 0.1;
-  if (val > 1) val = 0;
-
-  console.log(`val: ${val} value: ${value}`);
+  console.log(`value: ${value}`);
  
-  led.writeSync(val);
+  led.writeSync(value);
 });
  
 process.on('SIGINT', () => {
