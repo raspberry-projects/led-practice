1528e00ccb4d7f3caab7f19e3d1064c95da02a26
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-07-13 13:49:02 -0600

    log en consola

--- led-and-button.js
+++ led-and-button.js
@@ -1,13 +1,22 @@
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
+
+console.log('Programa iniciado, presiona el botón para encender el led.');
+
+val = 0;
  
 button.watch((err, value) => {
   if (err) {
     throw err;
   }
+
+  val += 0.1;
+  if (val > 1) val = 0;
+
+  console.log(`val: ${val} value: ${value}`);
  
-  led.writeSync(value);
+  led.writeSync(val);
 });
  
 process.on('SIGINT', () => {
